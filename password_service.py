af55e0d893a887ed380282516057990bee4704cb
oldmansaturn <dalton.herriman76@icloud.com>
2025-07-27 13:55:46 -0500

    added pepper from .env file

--- password_service.py
+++ password_service.py
@@ -1,15 +1,17 @@
 from argon2 import PasswordHasher, exceptions
+import os
 
+PEPPER = os.getenv("APP_PEPPER", "SomeVerySecretRandomKey")
 class PasswordService:
     def __init__(self):
         self.ph = PasswordHasher()  # Uses Argon2id by default
 
     def hash_password(self, password: str) -> str:
-        return self.ph.hash(password)
+        return self.ph.hash(password + PEPPER)
 
     def verify_password(self, hashed_password: str, password: str) -> bool:
         try:
-            self.ph.verify(hashed_password, password)
+            self.ph.verify(hashed_password, password + PEPPER)
             return True
         except exceptions.VerifyMismatchError:
             return False
